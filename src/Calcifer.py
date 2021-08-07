52ff4a3eaf1c6055f72c2ff3050b915dc6472791
Alister Haseldine <alister.haseldine@gmail.com>
2021-08-08 03:08:09 +0930

    Add new 8ball command. Chnage from using .format to f strings.

--- src/Calcifer.py
+++ src/Calcifer.py
@@ -7,12 +7,14 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 cwd = os.getcwd()
 logging.basicConfig(filename='logs.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(name)s: %(message)s', datefmt='%d/%m/%y %I:%M:%S %p')
 
+bot_version = os.getenv("VERSION")
+
 # Bot stuff
 description = '''A python remake of Calcifer, my C# discord bot.'''
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='~', description=description, intents=intents, case_insesnsitive=True)
+bot = commands.Bot(command_prefix='/', description=description, intents=intents, case_insesnsitive=True)
 
 # Grab all the Cogs
 for filename in os.listdir(f"{cwd}\\src\\Cogs"):
@@ -22,10 +24,10 @@ for filename in os.listdir(f"{cwd}\\src\\Cogs"):
 @bot.event
 async def on_ready():
     logging.info('Login Started')
-    print('Logged in as {bot.user.name} UID: {bot.user.id}.')
+    print(f'Logged in as {bot.user.name}. UID: {bot.user.id}.')
     logging.info('Login Completed')
     print('--------')
-    await bot.change_presence(activity=discord.Game(name='Version 0.1, now running on Python!'))
+    await bot.change_presence(activity=discord.Game(name=f'Version {bot_version}'))
 
 # Listen for command errors
 @bot.event
@@ -34,16 +36,16 @@ async def on_command_error(ctx, error):
         pass # Fail silently
 
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('You seem to be missing an argument for the command {ctx.command}. Please try again.')
+        await ctx.send(f'You seem to be missing an argument for the command {ctx.command}. Please try again.')
     
     if isinstance(error, commands.CommandInvokeError):
         await ctx.send('Command Invoke Error. Something fucked up.')
-        logging.error('Command Invoke Error in command {ctx.command}!')
+        logging.error(f'Command Invoke Error in command {ctx.command}!')
     
     if isinstance(error, commands.DisabledCommand):
-        await ctx.send('The {ctx.command} command has been disabled.')
+        await ctx.send(f'The {ctx.command} command has been disabled.')
 
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send('You dont have permission to run {ctx.command}!')
+        await ctx.send(f'You dont have permission to run {ctx.command}!')
     
 bot.run(TOKEN)
